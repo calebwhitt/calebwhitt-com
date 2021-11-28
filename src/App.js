447254e64de17e5609fda4ea793cf36fc7bfc4ad
Caleb Whittington <caleb.whittington@kurtgeiger.com>
2021-11-28 15:55:16 +0000

    Updating colour scheme and adding homepage text. Not currently mobile friendly site

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Caleb Whittington</h1>
-        <h3>Security Engineer</h3>
-      </header>
+      <div className="Title">        
+          <h1>Caleb Whittington</h1>
+          <hr/>
+          <h2>Security Engineer</h2>
+      </div>
+
+      <div className="Main-content">
+        <p>
+        I'm an industrious and highly-motivated individual, demonstrated in having moved to London at 18 years old to start my career as an Apprentice Test Engineer at <a target="_blank" href="https://www.kurtgeiger.com">Kurt Geiger</a>.<br/>
+        Before my apprenticeship finished, I was promoted to Junior Test Engineer, followed closely by a promotion to Security Engineer.<br/><br/>
+        Entirely self-taught, I code with Python, JavaScript, Ruby and PHP daily. Plenty of AWS experience too.
+        </p>
+      </div>
+      
     </div>
   );
 }
