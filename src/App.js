f1665a7279616eff009ca9ea07028f71329f6b05
Caleb Whittington <caleb.whittington@kurtgeiger.com>
2022-01-30 18:55:07 +0000

    Adding more text to homepage. Unfinished button being committed too.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="Title">        
-          <h1>Caleb Whittington</h1>
-          <hr/>
-          <h2>Security Engineer</h2>
-      </div>
+    return (
+        <div className='App'>
+            <div className='Wrapper'>
+                <div className='Text-content'>
+                    <p>Programming with Python, JavaScript, PHP and Ruby daily.<br/>I protect application code hosted on AWS full time, currently working as a Security Engineer at <a target='_blank' href='https://www.kurtgeiger.com'>Kurt Geiger</a>.</p>
+                    <p>Security should be non-obtrusive. I part-manage a team, architect AWS infrastructure, work with suppliers, monitor logs, respond to incidents- the list goes on.</p>
+                    <p>More recently, my main focus has been to create and implement SAST, DAST, SCA and IaC linting tools for CI/CD pipelines.</p>
 
-      <div className="Main-content">
-        <p>
-        I'm an industrious and highly-motivated individual, demonstrated in having moved to London at 18 years old to start my career as an Apprentice Test Engineer at <a target="_blank" href="https://www.kurtgeiger.com">Kurt Geiger</a>.<br/>
-        Before my apprenticeship finished, I was promoted to Junior Test Engineer, followed closely by a promotion to Security Engineer.<br/><br/>
-        Entirely self-taught, I code with Python, JavaScript, Ruby and PHP daily. Plenty of AWS experience too.
-        </p>
-      </div>
-      
-    </div>
+                    <p>Any questions? Send me an email:<br/><a href='mailto:calebwhitt@outlook.com'>calebwhitt@outlook.com</a></p>
+
+                    {/*    <button className="Email-button" role="button">calebwhitt@outlook.com</button>*/}
+                    {/*</a>*/}
+                </div>
+
+                <div className='Title'>
+                    <h1>Caleb Whittington</h1>
+                    <h2>SECURITY ENGINEER</h2>
+                </div>
+            </div>
+        </div>
   );
 }
 
